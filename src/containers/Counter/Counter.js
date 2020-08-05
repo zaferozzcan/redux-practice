76cb069740d454b0b8ebb3ee391c5a77fed72144
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-08-04 21:30:01 -0400

    Add mapStatetoProps and mapDispacth to props

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -22,16 +23,15 @@ class Counter extends Component {
             case 'sub':
                 this.setState((prevState) => { return { counter: prevState.counter - value } })
                 break;
-            default:
-                return action
         }
     }
 
     render() {
+        console.log(this.props);
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler('inc')} />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler('dec')} />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler('add', 5)} />
                 <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler('sub', 5)} />
@@ -40,4 +40,16 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({ type: 'INCREMENT' })
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
