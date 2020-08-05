c2750e7994f4f8c52c92f2a4ff8b04c736b39fac
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-08-04 22:33:38 -0400

    Add other functionalities

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -9,22 +9,7 @@ class Counter extends Component {
         counter: 0
     }
 
-    counterChangedHandler = (action, value) => {
-        switch (action) {
-            case 'inc':
-                this.setState((prevState) => { return { counter: prevState.counter + 1 } })
-                break;
-            case 'dec':
-                this.setState((prevState) => { return { counter: prevState.counter - 1 } })
-                break;
-            case 'add':
-                this.setState((prevState) => { return { counter: prevState.counter + value } })
-                break;
-            case 'sub':
-                this.setState((prevState) => { return { counter: prevState.counter - value } })
-                break;
-        }
-    }
+
 
     render() {
         console.log(this.props);
@@ -32,9 +17,9 @@ class Counter extends Component {
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler('dec')} />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler('add', 5)} />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler('sub', 5)} />
+                <CounterControl label="Decrement" clicked={() => this.props.onDecrementCounter()} />
+                <CounterControl label="Add 5" clicked={() => this.props.onAdd()} />
+                <CounterControl label="Subtract 5" clicked={() => this.props.onSub()} />
             </div>
         );
     }
@@ -48,7 +33,11 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({ type: 'INCREMENT' })
+        onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
+        onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
+        onAdd: () => dispatch({ type: 'ADD' }),
+        onSub: () => dispatch({ type: 'SUB' }),
+
     };
 };
 
